ce602eb0486052e7d2032f0abd02d4481d3f65c4
Abd0Khalifa <abdoali345757@gmail.com>
2025-01-30 22:41:44 +0200

    no changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 import Home from "./Pages/Home/Home";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
-import CourseDetails from './Pages/CourseDetails/CourseDetails.js';
+import CourseDetails from "./Pages/CourseDetails/CourseDetails.js";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
-      <Footer />    
-        
+      <Footer />
     </>
   );
 }
