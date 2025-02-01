9fc809f4b1df8f8d4326bb090475c14aa39dfba1
Abd0Khalifa <abdoali345757@gmail.com>
2025-02-01 19:47:15 +0200

    finish pathes in appJs

--- src/App.js
+++ src/App.js
@@ -29,7 +29,15 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/sDashboard" element={<StudentDashboardPage />} />
-        
+        <Route path="addCourse" element={<AddCourse />} />
+        <Route path="/browseCourses" element={<BrowseCourses />} />
+        <Route path="/courseDetails" element={<CourseDetails />} />
+        <Route path="/iDashboard" element={<InstractorDashboardPage />} />
+        <Route path="/iProfile" element={<InstractorProfilePage />} />
+        <Route path="manageCourses" element={<ManageCourses />} />
+        <Route path="/myCourses" element={<MyCourses />} />
+        <Route path="/sProfile" element={<StudentProfilePage />} />
+        <Route path="/sProgress" element={<StudentProgress />} />
       </Routes>
     </Router>
     </>
