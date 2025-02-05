a5e7a8320be9a2de57af80918d7ca40ae9a2a339
Ehab Mansour <mansehab990@gmail.com>
2025-02-06 01:06:25 +0200

    fix css profile page

--- src/Pages/StudentProfilePage/StudentProfilePage.js
+++ src/Pages/StudentProfilePage/StudentProfilePage.js
@@ -8,47 +8,49 @@ import StudentStatistics from "../../Components/StudentStatistics/StudentStatist
 import StudentHeader from "../../Components/StudentHeader/StudentHeader";
 
 const StudentProfilePage = () => {
-    const [isEditing, setIsEditing] = useState(false); 
-    const [userData, setUserData] = useState({}); 
+  const [isEditing, setIsEditing] = useState(false);
+  const [userData, setUserData] = useState({});
 
-    const handleEdit = (data) => {
-        setUserData(data); 
-        setIsEditing(true); 
-    };
+  const handleEdit = (data) => {
+    setUserData(data);
+    setIsEditing(true);
+  };
 
-    const handleSave = async (updatedData) => {
-        try {
-            console.log("Updated Data:", updatedData);
-            setIsEditing(false); 
-        } catch (error) {
-            console.error("Error updating user data:", error);
-        }
-    };
+  const handleSave = async (updatedData) => {
+    try {
+      console.log("Updated Data:", updatedData);
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Error updating user data:", error);
+    }
+  };
 
-    return (
-        <div className="min-h-screen flex bg-custom-dark">
-            <StudentSidebarProfile />
-            <main className="flex-1 md:ml-64 text-white">
-                <StudentHeader />
-                <br />
-                {isEditing ? (
-                    <StudentProfileForm userData={userData} onSave={handleSave} />
-                ) : (
-                    <StudentProfileOverview onEdit={handleEdit} />
-                )}
+  return (
+    <div className="min-h-screen flex bg-custom-dark">
+      <StudentSidebarProfile />
+      <main className="flex-1 md:ml-64 text-white">
+        <StudentHeader />
+        <br />
+        <div className="p-5">
+          {isEditing ? (
+            <StudentProfileForm userData={userData} onSave={handleSave} />
+          ) : (
+            <StudentProfileOverview onEdit={handleEdit} />
+          )}
 
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                    <div className="lg:col-span-2 space-y-8">
-                        {!isEditing && <StudentStatistics />}
-                    </div>
-                    <div className="space-y-8">
-                        <StudentProfileAccountSettings />
-                        <StudentProfileConnectedAccounts />
-                    </div>
-                </div>
-            </main>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            <div className="lg:col-span-2 space-y-8">
+              {!isEditing && <StudentStatistics />}
+            </div>
+            <div className="space-y-8">
+              <StudentProfileAccountSettings />
+              <StudentProfileConnectedAccounts />
+            </div>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 };
 
 export default StudentProfilePage;
