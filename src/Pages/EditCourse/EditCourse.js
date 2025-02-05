d00d694245302b23ad3c47735acd80dc1ffc5a27
Ehab Mansour <mansehab990@gmail.com>
2025-02-05 03:42:20 +0200

    fix add course

--- src/Pages/EditCourse/EditCourse.js
+++ src/Pages/EditCourse/EditCourse.js
@@ -131,7 +131,7 @@ const EditCourse = () => {
 
   return (
     <div className="min-h-screen flex bg-custom-dark">
-      {/* <InstractorSidebarProfile /> */}
+      <InstractorSidebarProfile />
       <main className="flex-1 md:ml-64 text-white">
         <InstractorHeader />
         <div className="container mx-auto px-4 py-8">
