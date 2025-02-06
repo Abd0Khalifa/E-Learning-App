bf79550adbede7de963f71eb29aa453e490da1e1
Abd0Khalifa <abdoali345757@gmail.com>
2025-02-06 04:03:01 +0200

    finish InstractorProfileform componant

--- src/Pages/InstractorProfilePage/InstractorProfilePage.js
+++ src/Pages/InstractorProfilePage/InstractorProfilePage.js
@@ -8,27 +8,50 @@ import InsatractorProfessionalLinks from "../../Components/InsatractorProfession
 import InstractorVerificationStatus from "../../Components/InstractorVerificationStatus/InstractorVerificationStatus";
 import InstractorStatistics from "../../Components/InstractorStatistics/InstractorStatistics";
 import InstractorHeader from "../../Components/InstractorHeader/InstractorHeader";
+import StudentProfileForm from "../../Components/StudentProfileForm/StudentProfileForm";
+import StudentProfileConnectedAccounts from "../../Components/StudentProfileConnectedAccounts/StudentProfileConnectedAccounts";
 
 const InstractorProfilePage = () => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [userData, setUserData] = useState({});
+  
+    const handleEdit = (data) => {
+      setUserData(data);
+      setIsEditing(true);
+    };
+  
+    const handleSave = async (updatedData) => {
+      try {
+        console.log("Updated Data:", updatedData);
+        setIsEditing(false);
+      } catch (error) {
+        console.error("Error updating user data:", error);
+      }
+    };
+  
     return (
         <div className="min-h-screen flex bg-custom-dark">
             <InstractorSidebarProfile />
             <main className="flex-1 md:ml-64 text-white">
             <InstractorHeader />
                 <br/>
-                <InstractorProfileOverview />
+                <div className="p-5">
+          {isEditing ? (
+            <StudentProfileForm userData={userData} onSave={handleSave} />
+          ) : (
+            <InstractorProfileOverview onEdit={handleEdit} />
+          )}
 
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                    <div className="lg:col-span-2 space-y-8">
-                        <InstractorProfileForm />
-                        <InstractorStatistics />
-                    </div>
-                    <div className="space-y-8">
-                        <InstractorProfileAccountSettings />
-                        <InsatractorProfessionalLinks />
-                        <InstractorVerificationStatus />
-                    </div>
-                </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            <div className="lg:col-span-2 space-y-8">
+              {!isEditing && <InstractorStatistics />}
+            </div>
+            <div className="space-y-8">
+              <InstractorProfileAccountSettings />
+              <StudentProfileConnectedAccounts />
+            </div>
+          </div>
+        </div>
             </main>
         </div>
     );
